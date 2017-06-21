058108128f1a2bf7fa1f1910b7dd2f4a30b17f44
vanzez <phn1910@gmail.com>
2017-06-21 17:46:23 +0700

    no message

--- FinalProject/app.js
+++ FinalProject/app.js
@@ -37,10 +37,15 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // catch 404 error:
+  if(err.status == 404){
+    res.render('404');
+  } else {
+      // render the error page
+      res.status(err.status || 500);
+      res.render('error');
+  }
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
 });
 
 module.exports = app;
